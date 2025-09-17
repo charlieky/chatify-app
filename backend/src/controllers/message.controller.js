5134a7698c304717583287ba0112797eea190c3b
charlieky <charliekyle795@gmail.com>
2025-09-17 13:57:33 +0700

    message edit

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -66,10 +66,7 @@ export const sendMessage = async (req, res) => {
 
     await newMessage.save();
 
-    const receiverSocketId = getReceiverSocketId(receiverId);
-    if (receiverSocketId) {
-      io.to(receiverSocketId).emit("newMessage", newMessage);
-    }
+   
 
     res.status(201).json(newMessage);
   } catch (error) {
